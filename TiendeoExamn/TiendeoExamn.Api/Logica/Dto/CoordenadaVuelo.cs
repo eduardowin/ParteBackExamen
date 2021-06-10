7cb209578a7ded857ce71d196b65c7e8c3fa4cd2
Eduardo Zuniga <lord_zeg25@hotmail.com>
2021-06-10 08:56:25 -0500

    data validation

--- TiendeoExamn/TiendeoExamn.Api/Logica/Dto/CoordenadaVuelo.cs
+++ TiendeoExamn/TiendeoExamn.Api/Logica/Dto/CoordenadaVuelo.cs
@@ -9,10 +9,14 @@ namespace TiendeoExamn.Api.Logica.Dto
     public class CoordenadaVuelo
     {
         [Required(ErrorMessage = "Debe ingresar la posicion X de la coordenada inicial")]
+        [Range(1, double.MaxValue, ErrorMessage = "La punto X de la coordenada inicial debe ser mayor de 1")]
         public int PuntoX { get; set; }
         [Required(ErrorMessage = "Debe ingresar la posicion Y de la coordenada inicial")]
+        [Range(1, double.MaxValue, ErrorMessage = "La punto Y de la coordenada inicial debe ser mayor de 1")]
+
         public int PuntoY { get; set; }
         [Required(ErrorMessage = "Debe ingresar la dirección de la coordenada inicial")]
+        [RegularExpression(@"[N,S,O,E]", ErrorMessage = "Debe ingresar un direccion valida, se acepta: N,S,E,O")]
         public string Direccion { get; set; }
     }
 }
--- TiendeoExamn/TiendeoExamn.Api/Logica/Dto/PeticionDto.cs
+++ TiendeoExamn/TiendeoExamn.Api/Logica/Dto/PeticionDto.cs
@@ -9,8 +9,10 @@ namespace TiendeoExamn.Api.Logica.Dto
     public class PeticionDto
     {
         [Required(ErrorMessage = "Debe ingresar la base del rectangulo")]
+        [Range(1, double.MaxValue, ErrorMessage = "La base del rectangulo debe ser mayor de 1")]
         public int PerimetroRectanguloBase { get; set; }
         [Required(ErrorMessage = "Debe ingresar la altura del rectangulo")]
+        [Range(1, double.MaxValue, ErrorMessage = "La altura del rectangulo debe ser mayor de 1")]
         public int PerimetroRectanguloAltura { get; set; }
         [Required, MinLength(1, ErrorMessage = "Debe ingresar almenos una instruccion de vuelo")]
         public List<InstruccionVueloDto> InstruccionesDto { get; set; }
